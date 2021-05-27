92dcb0c7d3dfa8d26505a17a7d63ce0de9195133
Alex <kpuk77@bk.ru>
2021-05-27 23:29:48 +0800

    Добавлены пустые представления, действия и начало пробы самостоятельной обработки ошибок

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -10,7 +10,14 @@ namespace WebStore.Controllers
         public HomeController(IConfiguration configuration) => _Configuration = configuration;
 
         public IActionResult Index() => View();
-
         public IActionResult SecondAction() => Content(_Configuration["Greetings"]);
+        public IActionResult Blog() => View();
+        public IActionResult BlogSingle() => View();
+        public IActionResult Cart() => View();
+        public IActionResult Checkout() => View();
+        public IActionResult ContactUs() => View();
+        public IActionResult Login() => View();
+        public IActionResult ProductDetails() => View();
+        public IActionResult Shop() => View();
     }
 }
--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -12,15 +12,14 @@ namespace WebStore
 
         public Startup(IConfiguration Configuration) => this.Configuration = Configuration;
 
-        public void ConfigureServices(IServiceCollection services)
-        {
+        public void ConfigureServices(IServiceCollection services) => 
             services.AddControllersWithViews();
-        }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment()) 
+            if (env.IsDevelopment())
                 app.UseDeveloperExceptionPage();
+            //app.UseExceptionHandler("/Errors");
 
             app.UseRouting();
 
@@ -36,6 +35,10 @@ namespace WebStore
                 endpoints.MapControllerRoute(
                     "employees",
                     "{controller=Employees}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
+                    "errors",
+                    "{controller=Errors}/{action=Index}/{id!})");
             });
         }
     }
