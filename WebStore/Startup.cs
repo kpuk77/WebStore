6dec788fdc854580a53cba8d08a32b2a27cf61fd
Alex <kpuk77@bk.ru>
2021-06-11 18:05:10 +0800

    Добавлен инициализатор БД: Проверка актуальности БД, в случае необходимости выполняется миграция. Удаление id номеров и последующий перенос данных из памяти в БД.

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -1,10 +1,14 @@
+using System;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+
 using WebStore.DAL.Context;
+using WebStore.Data;
 using WebStore.Services;
 using WebStore.Services.Interfaces;
 
@@ -20,14 +24,20 @@ namespace WebStore
         {
             services.AddRazorPages().AddRazorRuntimeCompilation();
             services.AddControllersWithViews();
+
+            services.AddDbContext<WebStoreDB>(opt =>
+                opt.UseSqlServer(_Configuration.GetConnectionString("MSSqlServer")));
+            services.AddTransient<WebStoreDBInitializer>();
+
             services.AddTransient<IEmployeesData, InMemoryEmployeesData>();
             services.AddSingleton<IProductData, InMemoryProductData>();
-            services.AddDbContext<WebStoreDB>(opt => 
-                opt.UseSqlServer(_Configuration.GetConnectionString("MSSqlServer")));
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider services)
         {
+            using (var scope = services.CreateScope())
+                scope.ServiceProvider.GetRequiredService<WebStoreDBInitializer>().Initialize();
+
             if (env.IsDevelopment())
                 app.UseDeveloperExceptionPage();
 
