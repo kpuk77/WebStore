3d06f508dd625ac4101dd230d8e119159d0fd67a
Alex <kpuk77@bk.ru>
2021-06-19 16:57:35 +0800

    Исправления ссылок на корзину. Хлам.

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -20,9 +20,7 @@ namespace WebStore.Controllers
 
         public IActionResult Blog() => View();
         public IActionResult BlogSingle() => View();
-        public IActionResult Cart() => View();
         public IActionResult Checkout() => View();
         public IActionResult ContactUs() => View();
-        public IActionResult ProductDetails() => View();
     }
 }
