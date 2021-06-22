69a36153a7a358c71fe6bc5a6ee6c3d427d8f0bf
Alex <kpuk77@bk.ru>
2021-06-23 00:50:45 +0800

    Профиль

---
+++ WebStore/Controllers/UserProfileController.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Services.Interfaces;
+using WebStore.ViewModels;
+
+namespace WebStore.Controllers
+{
+    [Authorize]
+    public class UserProfileController : Controller
+    {
+        public IActionResult Index() => View();
+
+        public async Task<IActionResult> Orders([FromServices] IOrderService orderService)
+        {
+            var orders = await orderService.GetOrders(User.Identity!.Name);
+
+            return View(orders.Select(o => new UserOrderViewModel()
+            {
+                Id = o.Id,
+                Name = o.Name,
+                Phone = o.Phone,
+                Address = o.Address,
+                TotalPrice = o.Items.Sum(i => i.TotalItemsPrice)
+            }));
+        }
+    }
+}
