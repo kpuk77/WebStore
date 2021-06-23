d5f01ecae64f3cf11449a876e180e1dcb348c2be
Alex <kpuk77@bk.ru>
2021-06-23 15:59:43 +0800

    Ох уж эта коварная скобочка..

--- WebStore.Domain/Entities/Product.cs
+++ WebStore.Domain/Entities/Product.cs
@@ -20,7 +20,7 @@ namespace WebStore.Domain.Entities
 
         public string ImageUrl { get; set; }
 
-        [Column(TypeName = "decimal(18,2")]
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
     }
 }
