73721ddcadfc905b8ea7b2de006efaabeb4a1927
Alex <kpuk77@bk.ru>
2021-06-29 15:12:28 +0800

    Сервисы, БД в WebAPI.

--- Services/WebStore.WebAPI/Startup.cs
+++ Services/WebStore.WebAPI/Startup.cs
@@ -1,22 +1,88 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+
+using System;
+
+using WebStore.DAL.Context;
+using WebStore.Domain.Entities.Identity;
+using WebStore.Interfaces.Services;
+using WebStore.Services.Data;
+using WebStore.Services.InCookies;
+using WebStore.Services.InSQL;
 
 namespace WebStore.WebAPI
 {
     public class Startup
     {
         private IConfiguration _Configuration { get; }
-        
+
         public Startup(IConfiguration configuration) => _Configuration = configuration;
-        
+
         public void ConfigureServices(IServiceCollection services)
         {
+            var dbSource = _Configuration["DBSource"];
+            switch (dbSource)
+            {
+                case "SQLite":
+                    services.AddDbContext<WebStoreDB>(opt => opt.UseSqlite(_Configuration.GetConnectionString("SQLite"),
+                        o => o.MigrationsAssembly("WebStore.DAL.Sqlite")));
+                    break;
+                case "MSSqlServer":
+                    services.AddDbContext<WebStoreDB>(opt =>
+                        opt.UseSqlServer(_Configuration.GetConnectionString("MSSqlServer")));
+                    break;
+            }
+            services.AddTransient<WebStoreDBInitializer>();
+
+            services.AddIdentity<User, Role>()
+                .AddEntityFrameworkStores<WebStoreDB>()
+                .AddDefaultTokenProviders();
+
+            services.Configure<IdentityOptions>(opt =>
+            {
+#if DEBUG
+                opt.Password.RequireDigit = false;
+                opt.Password.RequireLowercase = false;
+                opt.Password.RequireUppercase = false;
+                opt.Password.RequireNonAlphanumeric = false;
+                opt.Password.RequiredLength = 3;
+                opt.Password.RequiredUniqueChars = 3;
+#endif
+
+                opt.User.RequireUniqueEmail = false;
+                opt.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+
+                opt.Lockout.AllowedForNewUsers = false;
+                opt.Lockout.MaxFailedAccessAttempts = 5;
+                opt.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(15);
+            });
+            services.ConfigureApplicationCookie(opt =>
+            {
+                opt.Cookie.Name = "WebStore38r";
+                opt.Cookie.HttpOnly = true;
+
+                opt.ExpireTimeSpan = TimeSpan.FromDays(7);
+
+                opt.LoginPath = "/Account/Login";
+                opt.LogoutPath = "/Account/Logout";
+                opt.AccessDeniedPath = "/Account/AccessDenied";
+
+                opt.SlidingExpiration = true;
+            });
+
+
             services.AddControllers();
-            services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebStore.WebAPI", Version = "v1" }));
+            services.AddSwaggerGen(c =>
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebStore.WebAPI", Version = "v1" }));
+            
+            services.AddScoped<IProductData, SqlProductData>();
+            services.AddScoped<ICartService, InCookiesCartService>();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
