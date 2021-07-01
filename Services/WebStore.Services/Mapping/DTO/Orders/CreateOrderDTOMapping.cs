de77baf8b3374b1d6571078d7bf85af35e85f9b3
Alex <kpuk77@bk.ru>
2021-07-01 14:48:21 +0800

    Проекция OrderDTO

---
+++ Services/WebStore.Services/Mapping/DTO/Orders/CreateOrderDTOMapping.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using WebStore.Domain.DTO.Orders;
+using WebStore.Domain.ViewModels;
+
+namespace WebStore.Services.Mapping.DTO.Orders
+{
+    public static class CreateOrderDTOMapping
+    {
+        public static IEnumerable<OrderItemDTO> ToDTO(this CartViewModel cart) =>
+            cart.Items.Select(p => new OrderItemDTO
+            {
+                ProductId = p.product.Id,
+                Price = p.product.Price,
+                Quantity = p.quantity
+            });
+
+        public static CartViewModel ToViewModel(this IEnumerable<OrderItemDTO> items) =>
+            new CartViewModel
+            {
+                Items = items.Select(p => (new ProductViewModel {Id = p.ProductId}, p.Quantity))
+            };
+    }
+}
---
+++ Services/WebStore.Services/Mapping/DTO/Orders/OrderDTOMapping.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using WebStore.Domain.DTO.Orders;
+using WebStore.Domain.Entities;
+
+namespace WebStore.Services.Mapping.DTO.Orders
+{
+    public static class OrderDTOMapping
+    {
+        public static OrderDTO ToDTO(this Order order) => order is null
+            ? null
+            : new OrderDTO
+            {
+                Id = order.Id,
+                Name = order.Name,
+                Phone = order.Phone,
+                Address = order.Address,
+                Date = order.TimeOrder,
+                Items = order.Items.ToDTO()
+            };
+
+        public static Order FromDTO(this OrderDTO order) => order is null
+            ? null
+            : new Order
+            {
+                Id = order.Id,
+                Name = order.Name,
+                Phone = order.Phone,
+                Address = order.Address,
+                TimeOrder = order.Date,
+                Items = order.Items.FromDTO().ToList()
+            };
+
+        public static IEnumerable<OrderDTO> ToDTO(this IEnumerable<Order> orders) => orders.Select(ToDTO);
+        public static IEnumerable<Order> FromDTO(this IEnumerable<OrderDTO> orders) => orders.Select(FromDTO);
+    }
+}
