35e6bf2c9bb99406c9d4f43b34672aae67178a3a
Alex <kpuk77@bk.ru>
2021-07-04 23:17:05 +0800

    Контроллеры и интерфейсы клиентов

---
+++ Services/WebStore.WebAPI/Controllers/Identity/RolesAPIController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using WebStore.DAL.Context;
+using WebStore.Domain.Entities.Identity;
+
+namespace WebStore.WebAPI.Controllers.Identity
+{
+    public class RolesAPIController
+    {
+        private readonly WebStoreDB _DB;
+        private readonly RoleStore<Role> _RoleStore;
+        public RolesAPIController(WebStoreDB db)
+        {
+            _DB = db;
+            _RoleStore = new RoleStore<Role>(db);
+        }
+
+        [HttpGet("roles/all")]
+        public async Task<IEnumerable<Role>> GetAllRoles() => await _RoleStore.Roles.ToArrayAsync();
+    }
+}
---
+++ Services/WebStore.WebAPI/Controllers/Identity/UsersAPIController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using WebStore.DAL.Context;
+using WebStore.Domain.Entities.Identity;
+
+namespace WebStore.WebAPI.Controllers.Identity
+{
+    public class UsersAPIController
+    {
+        private readonly WebStoreDB _DB;
+        private readonly UserStore<User, Role, WebStoreDB> _UserStore;
+
+        public UsersAPIController(WebStoreDB db)
+        {
+            _DB = db;
+            _UserStore = new UserStore<User, Role, WebStoreDB>(db);
+        }
+
+        [HttpGet("users/all")]
+        public async Task<IEnumerable<User>> GetAllUsers() => await _UserStore.Users.ToArrayAsync();
+    }
+}
