3fe042e56e48e6337dbbe77d629945f005cc3f12
Alex <kpuk77@bk.ru>
2021-07-09 11:55:48 +0800

    Создание системы логирования Log4Net

---
+++ Common/WebStore.Logger/Log4NetFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.Extensions.Logging;
+
+namespace WebStore.Logger
+{
+    public static class Log4NetFactory
+    {
+        private static string CheckPath(string filePath)
+        {
+            if (string.IsNullOrEmpty(filePath))
+                throw new ArgumentNullException("Не указан путь к файлу конфигурации");
+
+            if (Path.IsPathRooted(filePath))
+                return filePath;
+
+            var assembly = Assembly.GetEntryAssembly();
+            var dir = Path.GetDirectoryName(assembly!.Location);
+
+            return Path.Combine(dir!, filePath);
+        }
+
+        public static ILoggerFactory AddLog4Net(this ILoggerFactory factory, string configuration = "log4net.config")
+        {
+            factory.AddProvider(new Log4NetProvider(CheckPath(configuration)));
+
+            return factory;
+        }
+    }
+}
