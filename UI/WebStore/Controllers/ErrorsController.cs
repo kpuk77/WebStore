b9018c179fca38ae45a6619b0a85e115f2116dae
Alex <kpuk77@bk.ru>
2021-07-13 14:36:18 +0800

    Хлам ErrorsControllerTests

--- UI/WebStore/Controllers/ErrorsController.cs
+++ UI/WebStore/Controllers/ErrorsController.cs
@@ -4,20 +4,6 @@ namespace WebStore.Controllers
 {
     public class ErrorsController : Controller
     {
-        #region Колхозный велосипед
-        
-        public IActionResult Index(int id)
-        {
-            //if (id == 404)
-            //    return Error404();
-
-            //else
-            //    return Content("Ошибко");
-            return View("Error404");
-        }
-
-        private IActionResult Error404() => View();
-
-        #endregion
+        public IActionResult Index() => View("Error404");
     }
 }
