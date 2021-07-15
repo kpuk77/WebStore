b2894ae42e124a26b2b73348873735d93579a797
Alex <kpuk77@bk.ru>
2021-07-15 16:07:20 +0800

    Перенос блогов на отдельный контроллер.

---
+++ UI/WebStore/Controllers/BlogController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index() => View();
+
+        public IActionResult Single() => View();
+    }
+}
