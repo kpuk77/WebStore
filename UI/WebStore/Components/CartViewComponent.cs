fb9222553bb083ae0a55b8e88b03cfc017e54872
Alex <kpuk77@bk.ru>
2021-07-19 10:52:58 +0800

    Визуальный компонент корзины

---
+++ UI/WebStore/Components/CartViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using WebStore.Interfaces.Services;
+
+namespace WebStore.Components
+{
+    public class CartViewComponent : ViewComponent
+    {
+        private readonly ICartService _CartService;
+
+        public CartViewComponent(ICartService cartService) => _CartService = cartService;
+
+        public IViewComponentResult Invoke()
+        {
+            var itemsCount = _CartService.GetViewModel().ItemsCount;
+
+            ViewBag.Count = itemsCount > 0 ? $"({itemsCount})" : null;
+
+            return View();
+        }
+    }
+}
